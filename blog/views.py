6a6a552afdb31634289ff2a5394e5a0795691cd4
Veronica Lourens <Veronica@lourens.cloud>
2022-04-26 18:19:31 +0000

    Add like and unlike buttons and view functionalities.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -66,3 +68,16 @@ class PostDetail(View):
             },
         )
         
+
+
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
